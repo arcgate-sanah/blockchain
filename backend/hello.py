ae05d3efc9564308ee354773831eb754d7566f12
arcgate-sanah <sanah.mathur@arcgate.com>
2023-12-28 18:36:50 +0530

    refund

---
+++ backend/hello.py
@@ -0,0 +1,27 @@
+import json
+import environ
+
+from web3 import Web3, HTTPProvider
+
+env = environ.Env()
+environ.Env.read_env()
+
+blockchain_address = env("BLOCKCHAIN_ADDRESS")
+
+web3 = Web3(HTTPProvider(blockchain_address))
+
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+compiled_contract_path = "build/contracts/HelloWorld.json"
+
+deployed_contract_address = "DEPLOYED_CONTRACT_ADDRESSES"
+
+with open(compiled_contract_path) as file:
+    contract_json = json.load(file)
+    contract_abi = contract_json["abi"]
+
+contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi)
+
+message = contract.functions.sayHello().call()
+
+print(message)

415e266f44b44ef4d8e7551108009c46690efa45
arcgate-sanah <sanah.mathur@arcgate.com>
2023-12-28 18:38:53 +0530

    refund

--- contracts/NftMinting.sol
+++ contracts/NftMinting.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MintableToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 1000000 * (10**uint256(decimals())));
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
